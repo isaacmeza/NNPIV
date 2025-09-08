36385fe95c647f4224d9cdba0e7471e42ee5bc3d
Isaac Meza <i.mezal.12@gmail.com>
2025-09-08 00:20:55 -0400

    Added fork reference

--- nnpiv/neuralnet/deepiv/__init__.py
+++ nnpiv/neuralnet/deepiv/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+Forked from the repository:
+https://github.com/jhartford/DeepIV
+By Jason Hartford on April 15, 2020
+"""
+
 __author__ = """Jason Hartford"""
 __email__ = 'jasonhar@cs.ubc.ca'
 __version__ = '0.1.0'
